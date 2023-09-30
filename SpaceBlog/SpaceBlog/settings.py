8671c7c8e47f3bc5fadb2f08a4c01c5163b309ad
k <kostneko30@gmail.com>
2023-09-30 08:19:45 -0400

    email confirm(almost done)

--- SpaceBlog/SpaceBlog/settings.py
+++ SpaceBlog/SpaceBlog/settings.py
@@ -134,6 +134,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = 'kostneko30@gmail.com'  
-EMAIL_HOST_PASSWORD = 'dhcc jlfk ydhn wddx'   
+EMAIL_HOST_USER = os.environ.get("spaceblog_email")
+EMAIL_HOST_PASSWORD = os.environ.get("spaceblog_password")   
 
